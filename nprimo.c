45a4bb8dfd6efb9ab312b2a35e126e80c3a1f411
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-04 16:30:27 -0500

    Dar primeras instrucciones

--- nprimo.c
+++ nprimo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(int argc, char const *argv)
 {
+    int num;
+    printf("Ingrese el numero\n");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        printf("El numero es primo\n");
+    }
+    else
+        printf("No es Primo");
 
     return 0;
 }
