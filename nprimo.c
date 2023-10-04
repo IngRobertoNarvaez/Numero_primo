8b5d1593dfc1e4687798e0b0e00ad8bd32e1f98e
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-10-04 16:37:07 -0500

    Se da el formato bien definido para corregir

--- nprimo.c
+++ nprimo.c
@@ -5,6 +5,29 @@ int main(int argc, char const *argv)
     int cnum = 0;
     printf("Ingrese el numero\n");
     scanf("%d", &num);
+    if (num <= 1)
+    {
+        cnum = 0;
+    }
+    else
+    {
+        for (int i = 2; i <= num / 2; ++i)
+        {
+            if (num % i == 0)
+            {
+                cnum = 0;
+                break;
+            }
+        }
+    }
 
+    if (cnum)
+    {
+        printf("%d es un numero primo.\n", num);
+    }
+    else
+    {
+        printf("%d no es un numero primo.\n", num);
+    }
     return 0;
 }
